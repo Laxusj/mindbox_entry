01b3c12ef8278cb0c11e8e68c0335501932ea3f3
DESKTOP-6VULG34\Laxus <laxusjee@gmail.com>
2023-08-31 07:18:02 +0400

    SQL задание

---
+++ FigureArea/TaskSQL.sql
@@ -0,0 +1,28 @@
+﻿DROP  table if exists Product_Category;
+Drop TABLE IF EXISTS Product;
+drop table IF EXISTS Category;
+
+create TABLE Product(
+  ID int not null PRIMARY KEY,
+  Name nvarchar(100) not null
+);
+
+CREATE TABLE Category(
+ ID int not null PRIMARY KEY,
+ Name nvarchar(100) not null
+);
+
+
+create table Product_Category(
+  ProductID int not null FOREIGN key (ProductID) REFERENCES Product (ID)
+  	on DELETE Cascade,
+  CategoryID int not null FOREIGN Key (CategoryID) REFERENCES Category (ID)
+  	on DELETE cascade
+);
+
+select p.Name, c.Name
+from Product p 
+left join Product_Category pc 
+	on pc.ProductID = p.ID
+left JOIN Category c
+	on c.ID = pc.CategoryID
